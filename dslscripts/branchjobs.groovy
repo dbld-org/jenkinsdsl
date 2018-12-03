e1d6c79a51e62a90ad27919d3174e233c07dd9fb
Dario Matonicki <matonicki@gmail.com>
2018-12-03 17:06:01 +0200

    PLAT-XXX- pull all repos form owner
    pull all repos form owner and create pipleine job for each branch

--- dslscripts/branchjobs.groovy
+++ dslscripts/branchjobs.groovy
@@ -1,28 +1,53 @@
-String basePath = 'multipleBranches'
+def owner = 'qualica'
 
-folder(basePath) {
-    description 'This example shows how to create a set of jobs for each github branch, each in its own folder.'
-}
 
-def project = 'qualica/flexifin-documents'
-def branchApi = new URL("https://api.github.com/repos/${project}/branches")
-def branches = new groovy.json.JsonSlurper().parse(branchApi.newReader())
-branches.each {
-    def branchName = it.name
-    def jobName = "${project}-${branchName}".replaceAll('/','-')
-    job("${basePath}/${jobName}") {
-        scm {
-              remote {
-                github("${project}/${branchName}", 'ssh')
-                credentials('jenkins-github')
+repoApi = new URL("https://api.github.com/orgs/${owner}/repos?per_page=200")
+repos =
+        new groovy.json.JsonSlurper()
+                .parse(repoApi.newReader(
+                requestProperties: [Authorization: "token f19bb0c7f0a7b4305c0d802aed8bc975ee515c90"]))
+
+
+repos.findAll { it.name.startsWith('flexifin-') }.each {
+    def repo = it.name
+    def project = it.full_name
+    // def branchApi = new URL(it.branches_url)
+
+    String basePath = "${repo}-branches"
+
+    folder(basePath) {
+        description "${repo} branch builds"
+    }
+
+
+    def branchApi = new URL("https://api.github.com/repos/${project}/branches")
+    def branches =
+            new groovy.json.JsonSlurper()
+                    .parse(branchApi.newReader(
+                    requestProperties: [Authorization: "token f19bb0c7f0a7b4305c0d802aed8bc975ee515c90"]))
+    branches.each {
+        def branchName = it.name
+        def jobName = "${repo}-${branchName}".replaceAll('/', '-')
+        pipelineJob("${basePath}/${jobName}") {
+            definition{
+                cpsScm{
+                    lightweight(lightweight = true)
+                    scm {
+                        git() {
+                            branch("${branchName}")
+                            remote {
+                                github("${project}", 'ssh')
+                                credentials('jenkins-github')
+                            }
+                        }
+                        triggers {
+
+                            scm('H/20 * * * *')
+                        }
+                    }
                 }
-            // git("git://github.com/${project}.git", branchName)
-        }
-        steps {
-            maven("test -Dproject.name=${project}/${branchName}")
+            }
         }
     }
+
 }
-
-
-          
