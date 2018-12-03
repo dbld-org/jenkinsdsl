0b0dbcf4c7a631113ae2039e62e28665a0b2b2d0
Dario Matonicki <matonicki@gmail.com>
2018-12-03 13:45:56 +0200

    Update branchjobs.groovy

--- dslscripts/branchjobs.groovy
+++ dslscripts/branchjobs.groovy
@@ -4,7 +4,7 @@ folder(basePath) {
     description 'This example shows how to create a set of jobs for each github branch, each in its own folder.'
 }
 
-def project = 'quidryan/aws-sdk-test'
+def project = 'qualica/flexifin-documents'
 def branchApi = new URL("https://api.github.com/repos/${project}/branches")
 def branches = new groovy.json.JsonSlurper().parse(branchApi.newReader())
 branches.each {
